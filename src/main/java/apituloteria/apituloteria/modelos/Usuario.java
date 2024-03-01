ad9f68023471f085710e2c6f274c43eebde6e6c6
Andres Felipe Graciano Monsalve <72512285+afgraciano@users.noreply.github.com>
2024-02-29 22:35:43 -0500

    COMMIT entidad variables tabla usuario

--- src/main/java/apituloteria/apituloteria/modelos/Usuario.java
+++ src/main/java/apituloteria/apituloteria/modelos/Usuario.java
@@ -1,10 +1,32 @@
 package apituloteria.apituloteria.modelos;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="usuario")
 public class Usuario {
 
-    @Override
+    @Column(name="idusuario")
+    private long id;
+
+    @Column(name="nombre")
+    private String nombre;
+
+    @Column(name="correo")
+    private String email;
+
+    @Column(name="clave")
+    private String clave;
+    
+    
+    @Column(name="identificacion")
+    private String identificacion;
+
+    /*@Override
     public String toString() {
         return "usuario []";
-    }
+    }*/
     
 }
